93705af5bd1ef19b4769bf39bae33f603e594537
Jeffry Gonzalez <jeff@hypertheory.com>
2021-04-13 11:16:36 -0400

    before break

---
+++ src/learningtypescript/0_types.spec.ts
@@ -0,0 +1,9 @@
+describe('data types in typescript', () => {
+    it('demo', () => {
+        const a = 10, b = 20;
+
+        const answer = a + b;
+
+        expect(answer).toBe(30);
+    });
+});
