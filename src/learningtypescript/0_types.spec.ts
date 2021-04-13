bb18057005ffc27f9f5a16a28027c619eb374f4f
Jeffry Gonzalez <jeff@hypertheory.com>
2021-04-13 14:47:07 -0400

    added eslint and did some code

--- src/learningtypescript/0_types.spec.ts
+++ src/learningtypescript/0_types.spec.ts
@@ -1,6 +1,6 @@
 describe('data types in typescript', () => {
   xit('demo', () => {
-    const a = 10, b = 20;
+    const a = 10; const b = 20;
 
     const answer = a + b;
 
@@ -10,9 +10,9 @@ describe('data types in typescript', () => {
   describe('declaring variables', () => {
 
     it('implicit typing', () => {
-      let x = 'Cat';
+      const x = 'Cat';
 
-      let y = 12;
+      const y = 12;
 
       expect(typeof x).toBe('string');
       expect(typeof y).toBe('number');
@@ -25,26 +25,61 @@ describe('data types in typescript', () => {
       let y: string;
       let z: number | string;
 
+
+      // eslint-disable-next-line prefer-const
       x = 42;
+
+      // eslint-disable-next-line prefer-const
       y = 'Dog';
       z = 42;
       z = 'Cat';
 
 
     });
+    it('using const', () => {
+      const x = 12;
+
+      expect(x).toBe(12);
+
+      // All const means is that you can't reassign the variable to something completely new.
+
+      const friends = ['Billy', 'Sean', 'Amy'];
+
+      friends[2] = 'Jessika';
+
+      // friends = ['Jim', 'Lee'];
+
+      const movie = {
+        title: 'Dune',
+        director: 'Lynch'
+      };
+
+      expect(movie.title).toBe('Dune');
+
+      movie.director = 'Denis Villeneauve';
+
+      // movie = {
+      //   title: 'Mullholand Drive',
+      //   director: 'Lynch'
+      // }
+
+    });
+
+
   });
   describe('literals in typescript', () => {
 
     describe('string literals', () => {
       it('can use double or single quotes', () => {
-        let x = 'Bird';
-        let y = "Bird";
+        const x = 'Bird';
+        // eslint-disable-next-line @typescript-eslint/quotes
+        const y = "Bird";
 
         expect(x).toEqual(y);
 
         //let author = 'Flannery O\'Connor';
-        let author = "Flannery O'Connor";
-        let quote = 'She said "Hello!".';
+        const author = 'Flannery O\'Connor';
+        const quote = 'She said "Hello!".';
       });
 
     });
