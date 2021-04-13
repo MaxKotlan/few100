9a246651690196b4b96440a3cf289f8d887162d0
Jeffry Gonzalez <jeff@hypertheory.com>
2021-04-13 15:17:28 -0400

    before break tuesday pm

--- src/learningtypescript/0_types.spec.ts
+++ src/learningtypescript/0_types.spec.ts
@@ -65,7 +65,18 @@ describe('data types in typescript', () => {
 
     });
 
+    it('why is var so bad anyhow', () => {
 
+      const age = 22;
+
+      if (age > 21) {
+        // eslint-disable-next-line no-var
+        var message = 'Old Enough';
+      }
+
+
+      expect(message).toBe('Old Enough');
+    });
   });
   describe('literals in typescript', () => {
 
@@ -79,11 +90,79 @@ describe('data types in typescript', () => {
 
         //let author = 'Flannery O\'Connor';
         const author = 'Flannery O\'Connor';
-        const quote = 'She said "Hello!".';
+        // eslint-disable-next-line @typescript-eslint/quotes
+        const quote = "She said \"Hello!\".";
+      });
+
+      it('back ticks', () => {
+        const drink = `Beer`;
+        expect(drink).toEqual('Beer');
+
+        const myLifeStory = `
+        Chapter 1.
+
+        It was a dark and stormy night.
+        So I got a beer.
+        `;
+
+        const details = `
+        <div>Name Here</div>
+        <p>OTher stuff </div>`;
+
+        const name = 'Amy';
+        const age = 42;
+
+        //const message = 'My friend ' + name + ' is ' + age + ' years old';
+        const message = `My friend ${name} is ${age} years old`;
+        expect(message).toBe('My friend Amy is 42 years old');
+
       });
 
     });
+
+
+    describe('number literals', () => {
+      it('there are various ways to represent a number', () => {
+
+
+        const bigNumber = 123_888_222_123;
+        const favoriteColor = 0xff; // base 16 hexadecimal
+        const someBits = 0b1010101;
+        const perms = 0o333; // Octal.
+
+        const ageAsString = '051.35';
+
+        const ageAsNumber = parseInt(ageAsString, 10);
+        expect(ageAsNumber).toBe(51);
+
+      });
+
+    });
+
   });
 
+  describe('booleans', () => {
+
+    it('has named booleans', () => {
+      let fullTime: boolean;
+
+      fullTime = true;
+
+      expect(fullTime).toBe(true);
+
+      fullTime = false;
+      expect(fullTime).toBe(false);
+
+      // fullTime = 0;  // nope. none of this nonsense.
+
+    });
+  });
+  it('literal unions', () => {
+    type AccountType = 'gold' | 'platinum' | 'standard';
+
+    const myAccount: AccountType = 'standard';
+
+    expect(myAccount).toBe('standard');
+  });
 });
 
